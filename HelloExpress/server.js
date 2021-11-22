9f46bd7b803bb593821b48ee67a64d0d1cd734d4
Michael Maines <Luckiestpet@gmail.com>
2021-11-21 22:18:56 -0500

    Faker API finished

---
+++ HelloExpress/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+// make sure these lines are above any app.get or app.post code blocks
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
+
+
+
+// req is short for request
+// res is short for response
+app.get("/api", (req, res) => {
+  res.send("Our express api server is now sending this over to the browser");
+});
+
+const server = app.listen(8000, () =>
+  console.log(`Server is locked and loaded on port ${server.address().port}!`)
+);
